ea3cb5b8f28a20834021ee1e238b165a38eb5044
CamilaCordova2578 <camiayecova@gmail.com>
2026-04-15 21:37:13 -0400

    Actualizando lo basico del servidor

---
+++ refugio-proyecto/server.js
@@ -0,0 +1,100 @@
+import http from 'node:http';
+//importando los datos de la bd ficticia
+import datos from './database.json' with {type : "json"};
+//para archivos estaticos y sus rutas
+import fs from 'node:fs';
+import path from 'node:path';
+import { error } from 'node:console';
+
+
+//Variable auxiliar para generar los IDs
+let indexId = 3;
+//base de nuestra URL
+const  base = "http://localhost:8080";
+
+const mascota1 = datos[0].id;
+console.log(mascota1)
+
+
+http.createServer((req, res) =>{
+    let body = [];
+    const {method, url, headers} = req;
+    const urlMascota = new URL(url, base);
+
+    
+
+    //1. Protocolo GET y ruta /
+    if( method === 'GET' && url === '/'){
+        //2.1. Obteniendo la ruta de nuestro archivo index.html
+        const rutaIndex = path.join(__dirname, 'public', 'index.html');
+        
+        //1.2. Leer el archivo .html, fs.readFile()
+        //1er parametro: La Ruta
+        //2do parametro: El formato (utf8 para texto)
+        //3er parametro: La funcion que se ejecuta al leer, parametros
+        //error y data el archivo
+
+        fs.readFile(rutaIndex, 'utf8', (err, data) => {
+
+            //2.3. Hubo algun erro por parte del servidor al leer el archivo
+            if (err) {
+                res.statusCode = 500; //Error interno
+                res.setHeader("Content-Type", "application/json");
+                res.end(JSON.stringify({error : "Error al cargar la pagina"}))
+                return;
+            }
+
+            //Si salio bien
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'text/html');
+            res.end(data);
+            return;
+        });
+    }
+
+    //2. Si se quiere acceder a nuestra api donde devolvemos json usamos esta validacion
+    if(!headers.authorization && url.includes('/api/')){
+        res.statusCode = 401;
+        res.setHeader("Content-Type","application/json");
+        res.end(JSON.stringify({error : "No existe autorizacion"}));
+        return;
+    }
+
+    
+    //3. Acceder a todas las mascotas que sean gatos y tengan 2 anios
+    let mascotaEncontrada = [];
+    const especie = urlMascota.searchParams.get('especie');
+    const edad = parseInt(urlMascota.searchParams.get('edad'));
+
+    if (method === 'GET' && urlMascota.pathname === "/api/mascotas") {
+
+        mascotaEncontrada = datos.filter(mascota => {
+
+            if (especie && edad) { //primer filtro para descartar lo demas
+                return mascota.especie === especie && mascota.edad === edad // if(mascota.especie === especie  && mascota.edad === edad) { return mascota};
+            }
+
+            if (especie) {
+                return mascota.especie === especie;
+            }
+
+            if (edad) {
+                return mascota.edad === edad;
+            }
+
+            return true; // si no hay filtros , se devuelve todo
+        });
+
+        if (mascotaEncontrada.length > 0) {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify({ data: mascotaEncontrada }));
+        } else {
+            res.statusCode = 404;
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify({ error: "No se pudo encontrar la mascota" }));
+        }
+    }
+
+
+}).listen(8080, () => {console.log("Corriendo en http://localhost:8080")});
